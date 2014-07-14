84f85536ecd0608b6dbe14a2d486b05e54909aba
Belousow Makc <lib.bmw@gmail.com>
2014-07-14 05:05:41 +0400

    Update bootstrap layout

--- application/tracks/handlers/main.py
+++ application/tracks/handlers/main.py
@@ -1,8 +1,10 @@
 from flask.views import MethodView
+from flask import render_template
 from google.appengine.api import users
 
 
 class MainView(MethodView):
     def get(self):
-        return 'Hello tracks! (HomeView) <a href="{}">login</a>'.format(
-            users.create_login_url())
+        # return 'Hello tracks! (HomeView) <a href="{}">login</a>'.format(
+        #    users.create_login_url())
+        return render_template('layout.html')
