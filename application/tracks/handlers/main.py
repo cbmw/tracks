1be89131bfc1f1aa7172e38354d7b045a71a434f
Belousow Makc <lib.bmw@gmail.com>
2014-07-14 07:06:20 +0400

    Update navbar

--- application/tracks/handlers/main.py
+++ application/tracks/handlers/main.py
@@ -7,4 +7,4 @@ class MainView(MethodView):
     def get(self):
         # return 'Hello tracks! (HomeView) <a href="{}">login</a>'.format(
         #    users.create_login_url())
-        return render_template('layout.html')
+        return render_template('layout.html', login_url=users.create_login_url())
