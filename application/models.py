83866b54fce3d691bd41c5b8e0196f3917866497
Belousow Makc <lib.bmw@gmail.com>
2014-07-21 13:48:19 +0400

    Update user model

--- application/models.py
+++ application/models.py
@@ -4,23 +4,31 @@ from application.app import login_manager
 
 
 class User(UserMixin, ndb.Model):
-    """User Model"""
+    '''User Model'''
 
-    created_at = ndb.DateTimeProperty(auto_now_add=True)
+    created = ndb.DateTimeProperty(auto_now_add=True)
+    email = ndb.StringProperty()
     google_user = ndb.UserProperty()
+    logined = ndb.DateTimeProperty()
     name = ndb.StringProperty()
     surname = ndb.StringProperty()
+    weight = ndb.IntegerProperty()
+    height = ndb.IntegerProperty()
 
     def get_id(self):
-        return unicode(self.key.id())
+        '''Return User id for using in flask-login'''
+        return self.key.id()
 
     @classmethod
-    def create(cls, google_user):
+    def by_google(cls, google_user):
+        '''Create User entity by Google User Api object'''
         user = User(google_user=google_user, name=google_user.nickname())
+        user.name = google_user.nickname()
+        user.email
         user.put()
         return user
 
 
 @login_manager.user_loader
-def get_user(id):
-    return User.get_by_id(id)
+def get_user(user_id):
+    return User.get_by_id(user_id)
