69d8219011671eeb13f4d076ab5051f4175d0c50
Belousow Makc <lib.bmw@gmail.com>
2014-07-27 15:06:25 +0400

    Implement ndb.Model json encoding

--- gaejson.py
+++ gaejson.py
@@ -1,25 +1,42 @@
+import datetime
 import json
-from google.appengine.api.users import User
-
-
-def user_encoder(obj):
-    return {'id': obj.user_id(),
-            'email': obj.email(),
-            'auth_domain': obj.auth_domain(),
-            'nickname': obj.nickname()}
-
-
-GAE_TYPE_ENCODER = {
-    User: user_encoder,
-}
+from google.appengine.api import users
+from google.appengine.ext import ndb
 
 
 class GaeJSONEncoder(json.JSONEncoder):
     """ Extend JSONEncoder for GAE types support """
 
+    def _datetime_encoder(obj):
+        '''ISO 8601 format'''
+        zone = '' if getattr(obj, 'tzinfo', True) else 'Z'
+        return obj.isoformat() + zone
+
+    def _user_encoder(obj):
+        '''google.appengine.api.users.User object json encoder'''
+        return {'id': obj.user_id(),
+                'email': obj.email(),
+                'auth_domain': obj.auth_domain(),
+                'nickname': obj.nickname()}
+
+    def _model_encoder(obj):
+        '''google.appengine.ext.ndb.Model and subclasses encoder'''
+        return obj.to_dict()
+
+    encoders = {
+        users.User: _user_encoder,
+        ndb.model.MetaModel: _model_encoder,
+        datetime.datetime: _datetime_encoder,
+    }
+
     def default(self, obj):
+        return self.get_encoder(obj)
+
+    def get_encoder(self, obj):
         obj_type = type(obj)
-        encoder = GAE_TYPE_ENCODER.get(obj_type)
+        if obj_type not in self.encoders and hasattr(obj, '__metaclass__'):
+            obj_type = obj.__metaclass__
+        encoder = self.encoders.get(obj_type)
         if encoder:
             return encoder(obj)
         return json.JSONEncoder.default(self, obj)
